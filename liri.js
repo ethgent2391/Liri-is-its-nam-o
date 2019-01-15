ebbffa9a2caea69c20a9c2f925f849f4cbf6c3c4
Ethan Gent <ethan.gent.2391@gmail.com>
2019-01-15 11:54:34 -0500

    finall commit 2

--- liri.js
+++ liri.js
@@ -4,7 +4,7 @@ var fs = require("fs");
 var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
-
+ 
 var args = process.argv;
 
 var srchinput = "";
